f93e8dc2c09d4884832a41f1059d4728d2af9121
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-03-13 09:46:54 +0900

    Update install.sh

--- vue/install.sh
+++ vue/install.sh
@@ -1,1 +1,9 @@
-exit;
+
+# https://bootstrap-vue.org/docs
+
+if ! [ -x "$(command -v npm)" ]; then
+  wget https://raw.githubusercontent.com/mugimugi555/ubuntu/main/install_nodejs.sh && bash install_nodejs.sh ;
+fi
+
+cd ;
+npm install vue bootstrap bootstrap-vue ;
