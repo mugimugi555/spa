a8153db9c50ab0695e544ed1a595aa45e82f55fe
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-03-13 09:52:35 +0900

    Update install.sh

--- react/install.sh
+++ react/install.sh
@@ -1,1 +1,9 @@
-exit;
+
+# https://react-bootstrap.github.io/getting-started/introduction/
+
+if ! [ -x "$(command -v npm)" ]; then
+  wget https://raw.githubusercontent.com/mugimugi555/ubuntu/main/install_nodejs.sh && bash install_nodejs.sh ;
+fi
+
+cd ;
+npm install react-bootstrap bootstrap@5.1.3
