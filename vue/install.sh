9da7122a78633bae8cc1df17b08142070d4987f8
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-04-04 06:57:16 +0900

    Update install.sh

--- vue/install.sh
+++ vue/install.sh
@@ -1,9 +1,27 @@
 
 # https://bootstrap-vue.org/docs
 
+#
 if ! [ -x "$(command -v npm)" ]; then
   wget https://raw.githubusercontent.com/mugimugi555/ubuntu/main/install_nodejs.sh && bash install_nodejs.sh ;
 fi
 
+#
+if ! [ -x "$(command -v yarn)" ]; then
+ wget https://raw.githubusercontent.com/mugimugi555/ubuntu/main/install_yarn.sh && bash install_yarn.sh ;
+fi
+
+#
 cd ;
-npm install vue bootstrap bootstrap-vue ;
+sudo echo ;
+sudo npm install -g @vue/cli ;
+
+#
+vue create my-vue-bootstrap-3 ;
+cd my-vue-bootstrap-3 ;
+
+#
+npm install vue bootstrap bootstrap-vue-3 ;
+
+#
+npm run serve ;
