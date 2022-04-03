ffae43d9ea5d3e6f6092f86c92ca0b2389be6153
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-04-04 08:18:19 +0900

    Update main.js

--- vue/vue3/src/main.js
+++ vue/vue3/src/main.js
@@ -3,6 +3,5 @@ import App from './App.vue'
 
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import BootstrapVue from 'bootstrap-vue-3'
 
-createApp(App).use(BootstrapVue).mount('#app');
+createApp(App).mount('#app');
